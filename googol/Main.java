06f69db84fa6c057522e69a8851f7c97b63a6a76
Luiz-Soft <lcfrf@ecomp.poli.br>
2023-03-12 01:25:15 +0000

    refactor: added multicast client and minor fixes

--- googol/Main.java
+++ googol/Main.java
@@ -1,25 +1,25 @@
 
-
 import java.net.URL;
 import multicastserver.MulticastServer;
 import webcrawler.Downloader;
+
 public class Main {
     public static void main(String[] args) {
         try {
-            //downloader parameters
+            // downloader parameters
             URL startingUrl = new URL("https://en.wikipedia.org/");
-           
-            //client and multicast server parameters
             int numThreads = 100;
+
+            // client and multicast server parameters
             int port = 4321;
             String hostAddress = "224.3.2.1";
-            
+
             Downloader downloader = new Downloader(startingUrl, numThreads, port, hostAddress);
             downloader.start();
 
             MulticastServer multicastServer = new MulticastServer(port, hostAddress);
             multicastServer.start();
-            
+
         } catch (Exception e) {
             System.out.println("Error starting downloader: " + e.getMessage());
         }
