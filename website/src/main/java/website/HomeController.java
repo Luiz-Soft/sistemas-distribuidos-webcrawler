a07ea0e5c94bad19934a3c98a4bd2887acf9fa98
Luiz-Soft <lcfrf@ecomp.poli.br>
2023-05-15 18:27:08 +0100

    feat: added input box to home html

--- website/src/main/java/website/HomeController.java
+++ website/src/main/java/website/HomeController.java
@@ -3,6 +3,8 @@ package website;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -10,6 +12,12 @@ public class HomeController {
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("message", "Hello, Thymeleaf!");
+        return "home";
+    }
+
+    @PostMapping("/submit")
+    public String submit(@RequestParam String myInput, Model model) {
+        model.addAttribute("message", "You entered: " + myInput);
         return "home";
     }
 }
